3fc4c0290a7c34e8330dbb9691f471b6c88d2ad2
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-01-05 15:16:47 +0100

    create div to populate screen

---
+++ index.js
@@ -0,0 +1,12 @@
+const div = document.querySelector(".container");
+
+// function to create grid
+function createGrid(){
+
+const numGrid = Number(prompt("choose a number from 1-100 to create a new grid!"));
+   
+    let squared = numGrid * numGrid;
+     for (i = 0; i < squared; i++){
+         let grid = document.createElement("div")
+     }
+}
