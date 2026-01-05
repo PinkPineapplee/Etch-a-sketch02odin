767f63b60ded68555af8b069685138691f420af0
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-01-05 15:59:00 +0100

    Feat : Create RandomHex

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ const numGrid = Number(prompt("choose a number from 1-100 to create a new grid!"
        createGrid()
      }
 }
+
+
+function generateRandomHex(){
+    let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,a,b,c,d,e,f];
+    let alhpa = [a,b,c,d,e,f]
+
+    let randomHex = function(){
+        for(i = 0; i < 6; i++){
+        let randomNum = Math.random(num)
+        }
+    }
+}
