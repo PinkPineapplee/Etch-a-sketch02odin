ca42bdc6af62b2f583dcf3fd6611748462d52574
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-01-05 15:44:34 +0100

    set attribute

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const div = document.querySelector(".container");
 
 // function to create grid
 function createGrid(){
-
+let grid
 const numGrid = Number(prompt("choose a number from 1-100 to create a new grid!"));
     let squared = numGrid * numGrid;
      for (i = 0; i < squared; i++){
-         let grid = document.createElement("div")
+         grid = document.createElement("div");
+         grid.setAttribute("style", "height: 100%;", "width: 100%;", "color: white;" )
      }
 
      try{
